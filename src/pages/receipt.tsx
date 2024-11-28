229014db66e5bd3291fc95a81c9d9bf83763ad32
Francisco Castellanos <juan.castellanos@grupotarahumara.com.mx>
2024-11-28 09:41:42 -0600

    SIgnature Added

--- src/pages/receipt.tsx
+++ src/pages/receipt.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink } from '@react-pdf/renderer'
+import React, { useState, useRef } from 'react'
+import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink, Image } from '@react-pdf/renderer'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Layout from '../components/Layout'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
-
+import SignatureCanvas from 'react-signature-canvas' // Importa SignatureCanvas
 
 const styles = StyleSheet.create({
   page: { padding: 30 },
@@ -20,10 +20,11 @@ const styles = StyleSheet.create({
     boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)'
   },
   row: { flexDirection: 'row', justifyContent: 'space-between', marginBottom: 5 },
-  inputLabel: { fontSize: 12, fontWeight: 'bold' }
+  inputLabel: { fontSize: 12, fontWeight: 'bold' },
+  signatureCanvasContainer: { border: '1px solid #ccc', padding: '10px', marginTop: '20px' }
 })
 
-const ActaPDF = ({ formData }) => (
+const ActaPDF = ({ formData, firmaBase64 }) => (
   <Document>
     <Page style={styles.page}>
       <View style={styles.logoSection}>
@@ -92,6 +93,14 @@ const ActaPDF = ({ formData }) => (
         <Text style={styles.inputLabel}>Sellado: {formData.sellado}</Text>
         <Text style={styles.inputLabel}>Número de serie: {formData.numeroSerie}</Text>
       </View>
+      {/* Firma en PDF */}
+      <View style={styles.block}>
+        <Text style={styles.inputLabel}>Firma:</Text>
+        {firmaBase64 && (
+          <Image src={firmaBase64} style={{ width: 150, height: 50 }} />
+        )}
+
+      </View>
     </Page>
   </Document>
 )
@@ -129,129 +138,151 @@ const ActaDeLlegada = () => {
     numeroSerie: ''
   })
 
+  const [firmaBase64, setFirmaBase64] = useState(null)
+
+  const signaturePadRef = useRef<any>(null) // Refs para el signature pad
+
   const handleInputChange = (e) => {
     const { name, value } = e.target
     setFormData((prevData) => ({ ...prevData, [name]: value }))
   }
 
+  const clearSignature = () => {
+    signaturePadRef.current.clear()
+    setFirmaBase64(null)
+  }
+
+  const saveSignature = () => {
+    const dataUrl = signaturePadRef.current.toDataURL()
+    setFirmaBase64(dataUrl)
+  }
+
   return (
     <Layout>
-    <ResizablePanelGroup direction='horizontal'>
-      <ResizablePanel defaultSize={50}>
-        <div style={{ padding: '20px' }}>
-          <h1>Acta de Llegada</h1>
+      <ResizablePanelGroup direction='horizontal'>
+        <ResizablePanel defaultSize={50}>
+          <div style={{ padding: '20px' }}>
+            <h1>Acta de Llegada</h1>
 
-          <h2>Fecha y Verificación</h2>
-          <label>Fecha: </label>
-          <Input type='date' name='fecha' value={formData.fecha} onChange={handleInputChange} />
+            {/* Formulario con campos de entrada */}
+            <h2>Fecha y Verificación</h2>
+            <label>Fecha: </label>
+            <Input type='date' name='fecha' value={formData.fecha} onChange={handleInputChange} />
+            <label>Inicio de verificación: </label>
+            <Input type='text' name='inicioVerificacion' value={formData.inicioVerificacion} onChange={handleInputChange} />
+            <label>Término de verificación: </label>
+            <Input type='text' name='terminoVerificacion' value={formData.terminoVerificacion} onChange={handleInputChange} />
+            <label>O.C.: </label>
+            <Input type='text' name='oc' value={formData.oc} onChange={handleInputChange} />
 
-          <label>Inicio de verificación: </label>
-          <Input type='text' name='inicioVerificacion' value={formData.inicioVerificacion} onChange={handleInputChange} />
+            <label>Proveedor: </label>
+            <Input type='text' name='proveedor' value={formData.proveedor} onChange={handleInputChange} />
 
-          <label>Término de verificación: </label>
-          <Input type='text' name='terminoVerificacion' value={formData.terminoVerificacion} onChange={handleInputChange} />
+            <label>Especie: </label>
+            <Input type='text' name='especie' value={formData.especie} onChange={handleInputChange} />
 
-          <label>O.C.: </label>
-          <Input type='text' name='oc' value={formData.oc} onChange={handleInputChange} />
+            <label>Variedades: </label>
+            <Input type='text' name='variedades' value={formData.variedades} onChange={handleInputChange} />
 
-          <label>Proveedor: </label>
-          <Input type='text' name='proveedor' value={formData.proveedor} onChange={handleInputChange} />
+            <label>Frío de descarga: </label>
+            <Input type='text' name='frioDescarga' value={formData.frioDescarga} onChange={handleInputChange} />
 
-          <label>Especie: </label>
-          <Input type='text' name='especie' value={formData.especie} onChange={handleInputChange} />
+            <label>Cajas recibidas: </label>
+            <Input type='text' name='cajasRecibidas' value={formData.cajasRecibidas} onChange={handleInputChange} />
 
-          <label>Variedades: </label>
-          <Input type='text' name='variedades' value={formData.variedades} onChange={handleInputChange} />
+            <h2>Transporte</h2>
+            <label>Línea transportista: </label>
+            <Input type='text' name='lineaTransportista' value={formData.lineaTransportista} onChange={handleInputChange} />
 
-          <label>Frío de descarga: </label>
-          <Input type='text' name='frioDescarga' value={formData.frioDescarga} onChange={handleInputChange} />
+            <label>Número de contenedor: </label>
+            <Input type='text' name='numeroContenedor' value={formData.numeroContenedor} onChange={handleInputChange} />
 
-          <label>Cajas recibidas: </label>
-          <Input type='text' name='cajasRecibidas' value={formData.cajasRecibidas} onChange={handleInputChange} />
+            <label>Placas camión: </label>
+            <Input type='text' name='placasCamion' value={formData.placasCamion} onChange={handleInputChange} />
 
-          <h2>Transporte</h2>
-          <label>Línea transportista: </label>
-          <Input type='text' name='lineaTransportista' value={formData.lineaTransportista} onChange={handleInputChange} />
+            <label>Placas caja: </label>
+            <Input type='text' name='placasCaja' value={formData.placasCaja} onChange={handleInputChange} />
 
-          <label>Número de contenedor: </label>
-          <Input type='text' name='numeroContenedor' value={formData.numeroContenedor} onChange={handleInputChange} />
+            <label>Chofer: </label>
+            <Input type='text' name='chofer' value={formData.chofer} onChange={handleInputChange} />
 
-          <label>Placas camión: </label>
-          <Input type='text' name='placasCamion' value={formData.placasCamion} onChange={handleInputChange} />
+            <h2>Condiciones de Transporte</h2>
+            <label>Temperatura de set point: </label>
+            <Input type='text' name='tempSetPoint' value={formData.tempSetPoint} onChange={handleInputChange} />
 
-          <label>Placas caja: </label>
-          <Input type='text' name='placasCaja' value={formData.placasCaja} onChange={handleInputChange} />
+            <label>Observaciones set point: </label>
+            <Input type='text' name='observacionesSetPoint' value={formData.observacionesSetPoint} onChange={handleInputChange} />
 
-          <label>Chofer: </label>
-          <Input type='text' name='chofer' value={formData.chofer} onChange={handleInputChange} />
+            <label>Temperatura de pantalla: </label>
+            <Input type='text' name='tempPantalla' value={formData.tempPantalla} onChange={handleInputChange} />
 
-          <h2>Condiciones de Transporte</h2>
-          <label>Temperatura de set point: </label>
-          <Input type='text' name='tempSetPoint' value={formData.tempSetPoint} onChange={handleInputChange} />
+            <label>Observaciones pantalla: </label>
+            <Input type='text' name='observacionesPantalla' value={formData.observacionesPantalla} onChange={handleInputChange} />
 
-          <label>Observaciones set point: </label>
-          <Input type='text' name='observacionesSetPoint' value={formData.observacionesSetPoint} onChange={handleInputChange} />
+            <label>Termógrafo: </label>
+            <Input type='text' name='termografo' value={formData.termografo} onChange={handleInputChange} />
 
-          <label>Temperatura de pantalla: </label>
-          <Input type='text' name='tempPantalla' value={formData.tempPantalla} onChange={handleInputChange} />
+            <label>Temperatura de origen: </label>
+            <Input type='text' name='tempOrigen' value={formData.tempOrigen} onChange={handleInputChange} />
 
-          <label>Observaciones pantalla: </label>
-          <Input type='text' name='observacionesPantalla' value={formData.observacionesPantalla} onChange={handleInputChange} />
+            <label>Temperatura de destino: </label>
+            <Input type='text' name='tempDestino' value={formData.tempDestino} onChange={handleInputChange} />
 
-          <label>Termógrafo: </label>
-          <Input type='text' name='termografo' value={formData.termografo} onChange={handleInputChange} />
+            <h2>Inspección de Transporte</h2>
+            <label>Limpio, libre de malos olores: </label>
+            <Input type='text' name='limpio' value={formData.limpio} onChange={handleInputChange} />
 
-          <label>Temperatura de origen: </label>
-          <Input type='text' name='tempOrigen' value={formData.tempOrigen} onChange={handleInputChange} />
+            <label>Caja cerrada, en buen estado: </label>
+            <Input type='text' name='cajaCerrada' value={formData.cajaCerrada} onChange={handleInputChange} />
 
-          <label>Temperatura de destino: </label>
-          <Input type='text' name='tempDestino' value={formData.tempDestino} onChange={handleInputChange} />
+            <label>Lona en buen estado: </label>
+            <Input type='text' name='lona' value={formData.lona} onChange={handleInputChange} />
 
-          <h2>Inspección de Transporte</h2>
-          <label>Limpio, libre de malos olores: </label>
-          <Input type='text' name='limpio' value={formData.limpio} onChange={handleInputChange} />
+            <label>Libre de fauna nociva: </label>
+            <Input type='text' name='fauna' value={formData.fauna} onChange={handleInputChange} />
 
-          <label>Caja cerrada, en buen estado: </label>
-          <Input type='text' name='cajaCerrada' value={formData.cajaCerrada} onChange={handleInputChange} />
+            <label>Carga en buen estado: </label>
+            <Input type='text' name='carga' value={formData.carga} onChange={handleInputChange} />
 
-          <label>Lona en buen estado: </label>
-          <Input type='text' name='lona' value={formData.lona} onChange={handleInputChange} />
+            <label>Seguridad de carga: </label>
+            <Input type='text' name='seguridadCarga' value={formData.seguridadCarga} onChange={handleInputChange} />
 
-          <label>Libre de fauna nociva: </label>
-          <Input type='text' name='fauna' value={formData.fauna} onChange={handleInputChange} />
+            <label>Sellado: </label>
+            <Input type='text' name='sellado' value={formData.sellado} onChange={handleInputChange} />
 
-          <label>Carga en buen estado: </label>
-          <Input type='text' name='carga' value={formData.carga} onChange={handleInputChange} />
+            <label>Número de serie: </label>
+            <Input type='text' name='numeroSerie' value={formData.numeroSerie} onChange={handleInputChange} />
 
-          <label>Seguridad de carga: </label>
-          <Input type='text' name='seguridadCarga' value={formData.seguridadCarga} onChange={handleInputChange} />
-
-          <label>Sellado: </label>
-          <Input type='text' name='sellado' value={formData.sellado} onChange={handleInputChange} />
-
-          <label>Número de serie: </label>
-          <Input type='text' name='numeroSerie' value={formData.numeroSerie} onChange={handleInputChange} />
-        </div>
-      </ResizablePanel>
-
-      <ResizableHandle />
-
-      <ResizablePanel>
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-          <PDFViewer width='100%' height='100%'>
-            <ActaPDF formData={formData} />
-          </PDFViewer>
-          <div style={{ padding: '10px', display: 'flex', justifyContent: 'center' }}>
-            <PDFDownloadLink document={<ActaPDF formData={formData} />} fileName='acta_de_llegada.pdf'>
-              <Button variant='primary'>Descargar PDF</Button>
-            </PDFDownloadLink>
+            <h2>Firma del Responsable</h2>
+            <div className={styles.signatureCanvasContainer}>
+              <SignatureCanvas
+                ref={signaturePadRef}
+                penColor='black'
+                canvasProps={{ width: 500, height: 200, className: 'signature-canvas' }}
+              />
+            </div>
+            <Button onClick={clearSignature}>Limpiar Firma</Button>
+            <Button onClick={saveSignature}>Guardar Firma</Button>
           </div>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+        </ResizablePanel>
+
+        <ResizableHandle />
+
+        <ResizablePanel>
+          <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+            <PDFViewer width='100%' height='100%'>
+              <ActaPDF formData={formData} firmaBase64={firmaBase64} />
+            </PDFViewer>
+            <div style={{ padding: '10px', display: 'flex', justifyContent: 'center' }}>
+              <PDFDownloadLink document={<ActaPDF formData={formData} firmaBase64={firmaBase64} />} fileName='acta_de_llegada.pdf'>
+                <Button variant='primary'>Descargar PDF</Button>
+              </PDFDownloadLink>
+            </div>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </Layout>
   )
 }
-
 
 export default ActaDeLlegada
